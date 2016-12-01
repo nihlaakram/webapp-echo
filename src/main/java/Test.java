c4c623cb7753613b1ad7d95b6c916511bb9e3d3d
nihlaakram <nihlaakram@yahoo.com>
2016-12-01 13:44:02 +0530

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,50 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+/*
+* Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+public class Test extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        // Set the response message's MIME type
+        response.setContentType("text/html;charset=UTF-8");
+        // Allocate a output writer to write the response message into the network socket
+
+        String test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        PrintWriter out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        try {
+                out.println(test);
+        } finally {
+            out.close();  // Always close the output writer
+        }
+    }
+
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
